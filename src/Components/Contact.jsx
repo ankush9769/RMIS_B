f82b183e931331fd1bffe0d035ff4a6d10a3c2c0
Mayank-I <mrmayanksharma45@gmail.com>
2024-12-05 15:02:55 +0530

    new component contact

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './style/Contact.css';
+
+
+function Contact() {
+  return (
+   
+<body>
+<div class="contact-details-card">
+    <h2 class="heading">Let’s Connect and Create Something Exceptional</h2>
+    <p class="body">
+         Have questions or ready to start your next big project? We’re here to help—reach out today!
+    </p>
+     <div class="contact-container">
+        <div class="contact-details">
+            <div class="detail">
+                <img src="Whatsapp.svg" alt="WhatsApp" class="icon" />
+                <a href="https://chat.whatsapp.com/BeyBKzccK0BCWVmfCxel2a" target="_blank" class="whatsapp-link">
+                    XXXXX-XX-XXXX
+                </a>
+            </div>
+            <div class="detail">
+                <img src="Instagram.svg" alt="Instagram" class="icon" />
+                <a href="https://www.instagram.com/ratnamaceits/profilecard/?igsh=MXFqamV0aWRtbTlkcA%3D%3D" target="_blank" class="instagram-link">
+                 @------------------
+                </a>
+            </div>
+            <div class="detail">
+                 <img src="facebook.svg" alt="Facebook" class="icon" />
+                <a >@------------------</a>
+            </div>
+            <div class="detail">
+                <img src="youtube.svg" alt="YouTube" class="icon" />
+                <a href="https://youtube.com/@ratnamaceitsolutions?si=e5NCla5FVZzcaaMV">
+                --------------------
+                </a>
+            </div>
+            <div class="detail">
+                <img src="Twitter.svg" alt="Twitter" class="icon" />
+                <a href="https://x.com/RatnaMaceITS?t=zciPKp55Q7iZkTl5bmoiUA&s=09">
+                @------------------
+                </a>
+            </div>
+            <div class="detail">
+                <img src="Threads.svg" alt="Threads" class="icon" />
+                <a href="https://www.threads.net/@ratnamaceits">
+                 @------------------
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="script.js"></script>
+</body>
+   
+  )
+}
+
+export default Contact
