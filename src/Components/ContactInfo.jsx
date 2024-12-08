a43f243c021d3a9319217d61a7dab22ae8d31bde
Manishr345 <manishr030405@gmail.com>
2024-12-08 17:53:51 +0530

    Displaying all cards in Industry page

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style/Contact.css';
+import './Style/Contact.css';
 
 
 function ContactInfo() {
--- src/Pages/Industry.jsx
+++ src/Pages/Industry.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Card from '../Components/Card';
+import array from '../Json/Industry.json';
+import './Style/Industry.css'
 
 export default function Industry() {
   
   return (
-    <div>
-      <Card/>
+    <div className='industry'>
+      <div className='indusGrid'>
+      {array.map((data) => {
+        return <Card icon={data.icon} title={data.title} description={data.description}/>
+      })}
+    </div>
     </div>
   )
 }
