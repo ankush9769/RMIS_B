573fb51846f4fe752547d5f30c75b7d503e657eb
Manishr345 <manishr030405@gmail.com>
2024-12-09 20:19:34 +0530

    Updated industry page for better view

--- src/Pages/Industry.jsx
+++ src/Pages/Industry.jsx
@@ -4,14 +4,14 @@ import array from '../Json/Industry.json';
 import './Style/Industry.css'
 
 export default function Industry() {
-  
+
   return (
     <div className='industry'>
       <div className='indusGrid'>
-      {array.map((data) => {
-        return <Card icon={data.icon} title={data.title} description={data.description}/>
-      })}
-    </div>
+        {array.map((data) => {
+          return <Card icon={data.icon} title={data.title} description={data.description} />
+        })}
+      </div>
     </div>
   )
 }
