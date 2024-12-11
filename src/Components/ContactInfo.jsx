28fb66b0374ce5df911d78c5d635985bda4ab282
Mayank-I <mrmayanksharma45@gmail.com>
2024-12-11 14:19:46 +0530

    logo added

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -12,35 +12,35 @@ function ContactInfo() {
      <div className="contact-container">
         <div className="contact-details">
             <div className="detail">
-                <img src="Whatsapp.svg" alt="WhatsApp" className="icon" />
+                <img src="../Whatsapp.svg" alt="WhatsApp" className="icon" />
                 <a href="https://chat.whatsapp.com/BeyBKzccK0BCWVmfCxel2a" target="_blank" className="cLink whatsapp-link">
                     XXXXX-XX-XXXX
                 </a>
             </div>
             <div className="detail">
-                <img src="Instagram.svg" alt="Instagram" className="icon" />
+                <img src="../Instagram_icon.svg" alt="Instagram" className="icon" />
                 <a href="https://www.instagram.com/ratnamaceits/profilecard/?igsh=MXFqamV0aWRtbTlkcA%3D%3D" target="_blank" className="cLink instagram-link">
                  @------------------
                 </a>
             </div>
             <div className="detail">
-                 <img src="facebook.svg" alt="Facebook" className="icon" />
+                 <img src="../facebook.svg" alt="Facebook" className="icon" />
                 <a href="" className='cLink' >@------------------</a>
             </div>
             <div className="detail">
-                <img src="youtube.svg" alt="YouTube" className="icon" />
+                <img src="../youtube.svg" alt="YouTube" className="icon" />
                 <a href="https://youtube.com/@ratnamaceitsolutions?si=e5NCla5FVZzcaaMV" className='cLink'>
                 @--------------------
                 </a>
             </div>
             <div className="detail">
-                <img src="Twitter.svg" alt="Twitter" className="icon" />
+                <img src="../Twitter.svg" alt="Twitter" className="icon" />
                 <a href="https://x.com/RatnaMaceITS?t=zciPKp55Q7iZkTl5bmoiUA&s=09" className='cLink'>
                 @------------------
                 </a>
             </div>
             <div className="detail">
-                <img src="Threads.svg" alt="Threads" className="icon" />
+                <img src="../Threads.svg" alt="Threads" className="icon" />
                 <a href="https://www.threads.net/@ratnamaceits" className='cLink'>
                  @------------------
                 </a>
