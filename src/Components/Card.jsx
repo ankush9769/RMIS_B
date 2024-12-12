e65880cd13800bba3e4669392efc03c1a493185c
Manishr345 <manishr030405@gmail.com>
2024-12-12 13:08:24 +0530

    Updated mobile view for Card Component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,12 +11,13 @@ export default function Card({ icon, title, description, mobileIcon }) {
                     </svg>
                 </div>
                 <div className="cardIconMobile">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 -960 960 960" width="100px" fill="#EA33F7">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#EA33F7">
                         <path d={`${mobileIcon}`} />
                     </svg>
                 </div>
             </div>
             <div className="cardDescription">
+                <p className="cardTitleMobile">{title}</p>
                 <p>{description}</p>
             </div>
             <div className="cardTitle">
