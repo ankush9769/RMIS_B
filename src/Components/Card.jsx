10fe4241870711296089f57e2696f577283605ba
Manishr345 <manishr030405@gmail.com>
2024-12-12 11:57:49 +0530

    Added hover animations to Card Component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,13 +3,17 @@ import './Style/Card.css';
 
 export default function Card({ icon, title, description, mobileIcon }) {
     return (
-        <div className='card'>
+        <div className="card">
             <div className="cardIconDiv">
                 <div className="cardIcon">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="40px" fill="#EA33F7"><path d={`${icon}`} /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="40px" fill="#EA33F7">
+                        <path d={`${icon}`} />
+                    </svg>
                 </div>
-                <div className='cardIconMobile'>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 -960 960 960" width="100px" fill="#EA33F7"><path d={`${mobileIcon}`} /></svg>
+                <div className="cardIconMobile">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 -960 960 960" width="100px" fill="#EA33F7">
+                        <path d={`${mobileIcon}`} />
+                    </svg>
                 </div>
             </div>
             <div className="cardDescription">
@@ -19,5 +23,5 @@ export default function Card({ icon, title, description, mobileIcon }) {
                 <p>{title}</p>
             </div>
         </div>
-    )
+    );
 }
