aa98bb49bb21819f518c59da5db0a3cf99f1030b
Ankush pal <155830977+ankush9769@users.noreply.github.com>
2024-12-14 23:11:53 +0530

    updated form component

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react'
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useLocation } from 'react-router-dom';
+import MobileForm from './MobileForm';
 
 const Form = () => {
 
@@ -52,21 +53,28 @@ const Form = () => {
     });
   };
   return (
-    <div key={location.pathname} className="contact">
-      <Contactinfo></Contactinfo>
-      <form className='form' data-aos='fade-up'>
-        <p className='headings'>We'd love to hear from you!</p>
-        <input className="input firstinput" name="name" value={formData.name} onChange={handleChange} type="text" placeholder='Enter your full name' />
-        <input className="input" name="email" value={formData.email} onChange={handleChange} type="email" placeholder='Enter your email address' />
-        <input className="input" name="phone" value={formData.phone} onChange={handleChange} type="tel" placeholder='Enter your phone number' />
-        <input className="input" name="inquiry" value={formData.inquiry} onChange={handleChange} type='text' placeholder="what's your inquiry about?" />
-        <textarea className='textarea' name="message" value={formData.message} onChange={handleChange} placeholder="type your message here..." />
-        <div className="buttondiv">
-          <button className='button1' onClick={handleReset}>R E S E T</button>
-          <button className='button2' onClick={handleSubmit}>S U B M I T</button>
+    <>
+      <div className='desktopForm'>
+        <div key={location.pathname} className="contact">
+          <Contactinfo></Contactinfo>
+          <form className='form' data-aos='fade-up'>
+            <p className='headings'>We'd love to hear from you!</p>
+            <input className="input firstinput" name="name" value={formData.name} onChange={handleChange} type="text" placeholder='Enter your full name' />
+            <input className="input" name="email" value={formData.email} onChange={handleChange} type="email" placeholder='Enter your email address' />
+            <input className="input" name="phone" value={formData.phone} onChange={handleChange} type="tel" placeholder='Enter your phone number' />
+            <input className="input" name="inquiry" value={formData.inquiry} onChange={handleChange} type='text' placeholder="what's your inquiry about?" />
+            <textarea className='textarea' name="message" value={formData.message} onChange={handleChange} placeholder="type your message here..." />
+            <div className="buttondiv">
+              <button className='button1' onClick={handleReset}>R E S E T</button>
+              <button className='button2' onClick={handleSubmit}>S U B M I T</button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      </div>
+      <div className="MobileForm">
+        <MobileForm/>
+      </div>
+    </>
   )
 }
 
---
+++ src/Components/MobileForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState, useEffect } from 'react'
+import './style/MobileForm.css'
+
+export default function MobileForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        inquiry: '',
+        message: ''
+      });
+    
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+    };
+    const handleReset = (event) => {
+        event.preventDefault();
+        setFormData({
+            name: '',
+            email: '',
+            phone: '',
+            inquiry: '',
+            message: ''
+        });
+    };
+    return (
+        <div className='mobileForm'>
+            <div className='container'>
+                {/* <div className="pacman">
+                <div className="pacman-eye"></div>
+            </div> */}
+                {/* <div className="speech-bubble">
+                <p>We'd love to hear from you!</p>
+            </div> */}
+                <p className='mobileH'>Have questions or ready to start your next big project? We're here to help—reach out today!</p>
+                <form>
+                    <input className='mobileInput' name="name" value={formData.name} onChange={handleChange} type="text" placeholder="Enter your full name" required />
+                    <input className='mobileInput' name="email" value={formData.email} onChange={handleChange} type="email" placeholder="Enter your email" required />
+                    <input className='mobileInput' name="phone" value={formData.phone} onChange={handleChange} type="tel" placeholder="Enter your phone number" />
+                    <input className='mobileInput' name="inquiry" value={formData.inquiry} onChange={handleChange} type="text" placeholder="What's your inquiry about?" />
+                    <textarea className='mobileArea' name="message" value={formData.message} onChange={handleChange} rows="4" placeholder="Type your message here..." required></textarea>
+                    <div className="fbuttons">
+                        <button type="reset" className="reset-btn" onClick={handleReset}>RESET</button>
+                        <button type="submit" className="submit-btn" onClick={handleSubmit}>SUBMIT</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
