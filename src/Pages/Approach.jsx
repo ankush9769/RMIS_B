df873717c2a0a9a972d37cc0773b734ff7cfefc7
Manishr345 <manishr030405@gmail.com>
2024-12-18 21:08:35 +0530

    Finalised project

--- src/Pages/Approach.jsx
+++ src/Pages/Approach.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BarGraph from '../Components/ApproachInfo';
-import InsightsPhase from '../Components/Insights';
-import Discovery from '../Components/Discovery';
-import BlueprintPhase from '../Components/Approach links/desktop/Blueprint';
-import LaunchingPhase from '../Components/Launch';
-import TrainingPhase from '../Components/Train';
-import BuildingPhase from '../Components/Approach links/desktop/Build';
-import DesigningPhase from '../Components/Design';
-import TestingPhase from '../Components/Test';
-import OptimizationPhase from '../Components/Optimize';
+// import InsightsPhase from '../Components/Insights';
+// import Discovery from '../Components/Discovery';
+// import BlueprintPhase from '../Components/Approach links/desktop/Blueprint';
+// import LaunchingPhase from '../Components/Launch';
+// import TrainingPhase from '../Components/Train';
+// import BuildingPhase from '../Components/Approach links/desktop/Build';
+// import DesigningPhase from '../Components/Design';
+// import TestingPhase from '../Components/Test';
+// import OptimizationPhase from '../Components/Optimize';
 import Circular from '../Components/Circular';
 import './Style/Approach.css'
 
